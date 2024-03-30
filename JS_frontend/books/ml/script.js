9b7b84741be865b48ce524e931d423809b92f341
Vignesh A J <117640780+vigneshAJ1503@users.noreply.github.com>
2024-03-30 11:39:37 +0530

    Add files via upload

---
+++ JS_frontend/books/ml/script.js
@@ -0,0 +1,7 @@
+var loginbutton = document.querySelector(".login-button")
+var ccontainer = document.getElementById("container")
+
+
+ccontainer.addEventListener("click", function() {
+    container.style.display = "block"
+})
